a2e327f2cac93226418655e476f6efb2d9a2da3c
Saurabh Singh <ss21xtar@gmail.com>
2024-10-15 07:08:04 +0530

    Created a navbar and added Hero section

---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I’m a passionate software developer with a strong foundation in data structures and algorithms, evidenced by my Knight rank on LeetCode and notable achievements in coding competitions. My experience with the MERN stack, along with certifications in React and Kotlin, equips me with a versatile skill set. I thrive on learning new technologies and enjoy tackling complex problems, always eager to push my limits and explore innovative solutions. My competitive spirit drives me to continuously improve and excel in the ever-evolving tech landscape.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have experience working with technologies like React, Node.js, MySQL, and MongoDB, as well as Android development using Kotlin. Additionally, I engage in competitive programming, which sharpens my problem-solving skills. I thrive in collaborative environments and enjoy tackling complex challenges to deliver high-quality solutions. Outside of coding, I stay active, explore new technologies, and contribute to open-source projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Maze Solver",
+    image: project1,
+    description:
+      "Dynamic maze generator and solver using BFS and DFS with real-time visualization.",
+    technologies: ["Java", "Swing"],
+  },
+  {
+    title: "Inn Sync",
+    image: project2,
+    description:
+      "Hotel management system with a dashboard for booking and cabin management.",
+    technologies: ["React", "Express", "MongoDB"],
+  },
+  {
+    title: "Math Game",
+    image: project3,
+    description:
+      "Educational math game for kids with random problem generation and score tracking.",
+    technologies: ["Kotlin"],
+  },
+  {
+    title: "ATM Management System",
+    image: project4,
+    description:
+      "GUI for ATM operations with balance, withdrawal, and deposit functionalities.",
+    technologies: ["Python", "Tkinter"],
+  },
+];
+
+export const CONTACT = {
+  address: "Yamuna Vihar Colony Prayagraj, Uttar Pradesh, India",
+  phoneNo: "+91 7973494678 ",
+  email: "saurabhsingh9637@gmail.com.com",
+};
